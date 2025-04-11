8efff31eb36a12642d24bb143a5443f5f2b9f464
Butterfly 16 <bousinamisi09@gmail.com>
2025-04-11 17:11:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "BUTTERFLY-MD~hs4WkZaQ#uZ7ZDjbsE1f9YPNzuZbZsHnTASIK9FCa6fg6cb_4qDo",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
